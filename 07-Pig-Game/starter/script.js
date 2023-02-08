e5d6caaa753ea6720d02337592091e7c54ac7981
EmmanuelC1 <emcastillo@csumb.edu>
2023-02-07 20:24:39 -0800

    Completed Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,49 +1,5 @@
 'use strict';
 
-const newGame = () => {
-  score0Element.textContent = 0;
-  score1Element.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  scoreCounter = 0;
-  activePlayer = 0;
-
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-
-  diceImg.classList.add('hidden');
-  btnRoll.disabled = false;
-  btnHold.disabled = false;
-};
-
-const handleRoll = diceRoll => {};
-
-const changeActivePlayer = () => {
-  scoreCounter = 0; //reset counter, get ready to track next player
-
-  //Player 0
-  if (!activePlayer) {
-    activePlayer = !activePlayer;
-    player0El.classList.remove('player--active');
-    player1El.classList.add('player--active');
-
-    current0El.textContent = 0;
-
-    //Player 1
-  } else {
-    activePlayer = !activePlayer;
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-
-    current1El.textContent = 0;
-  }
-};
-
-const winner = player => {
-  //change playerXEl to black
-  //disable buttons
-};
-
 //Selecting Elements
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
@@ -53,7 +9,6 @@ const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
 const diceImg = document.querySelector('.dice');
-
 const btnNewGame = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -61,41 +16,83 @@ const btnHold = document.querySelector('.btn--hold');
 //Starting Condition Global Variables
 let scoreCounter;
 let activePlayer;
+let scores; //used for scoreElements
+
+const newGame = () => {
+  scores = [0, 0];
+  scoreCounter = 0;
+  activePlayer = 0;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceImg.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  btnRoll.disabled = false;
+  btnHold.disabled = false;
+};
 newGame();
+
+const changeActivePlayer = () => {
+  //reset current element score and counter
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  scoreCounter = 0;
+
+  //switch active player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  //switch active player class
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+const displayWinner = () => {
+  //add winnner class to winning player, and remove active player class
+  document.querySelector(`.player--${activePlayer}`).classList.add('player--winner'); //prettier-ignore
+  document.querySelector(`.player--${activePlayer}`).classList.remove('player--active'); //prettier-ignore
+  diceImg.classList.add('hidden');
+
+  //disable buttons
+  btnRoll.disabled = true;
+  btnHold.disabled = true;
+};
 
 //Event Listeners
 btnNewGame.addEventListener('click', newGame);
 
 btnRoll.addEventListener('click', () => {
-  //Generate random dice roll
+  //generate random dice roll
   const diceRoll = Math.floor(Math.random() * 6 + 1);
-  console.log(diceRoll);
 
-  //Display dice
+  //display dice
   diceImg.classList.remove('hidden');
   diceImg.src = `dice-${diceRoll}.png`;
 
-  //Check for rolled 1
+  //check for rolled 1
   if (diceRoll !== 1) {
-    //Add diceRoll to current score
+    //add diceRoll to current score element and display in current element
     scoreCounter += diceRoll;
-    activePlayer ? (current1El.textContent = scoreCounter) : (current0El.textContent = scoreCounter); //prettier-ignore
+    document.querySelector(`#current--${activePlayer}`).textContent = scoreCounter; //prettier-ignore
   } else {
-    //Change active player
     changeActivePlayer();
   }
 });
 
 btnHold.addEventListener('click', () => {
-  activePlayer ? (score1Element.textContent = Number(score1Element.textContent) + scoreCounter) : (score0Element.textContent = Number(score0Element.textContent) + scoreCounter); //prettier-ignore
-  if (Number(score0Element.textContent) >= 20) {
-    winner(player0El); //FIX ME figure out what to send as a parameter
-    console.log('Player 0 wins');
-  } else if (Number(score1Element.textContent) >= 20) {
-    winner(player1El); //FIX ME figure out what to send as a parameter
-    console.log('Player 1 wins');
+  //add current score to active player's score
+  scores[activePlayer] += scoreCounter;
+  document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer]; //prettier-ignore
+
+  //check if player's score is >= 100. If true, display winner, else switch players
+  if (scores[activePlayer] >= 100) {
+    displayWinner();
   } else {
-    changeActivePlayer(); //BUG not changing avtive player after hold button is pressed
-    console.log('hold'); // Maybe it is working as intended.
+    changeActivePlayer();
   }
 });
