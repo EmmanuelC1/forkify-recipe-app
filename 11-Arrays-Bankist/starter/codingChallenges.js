8633e357f32c9bbc24a153e4480f282dd45a8958
EmmanuelC1 <emcastillo@csumb.edu>
2023-02-28 23:55:39 -0800

    coding challenge #2

--- 11-Arrays-Bankist/starter/codingChallenges.js
+++ 11-Arrays-Bankist/starter/codingChallenges.js
@@ -45,3 +45,50 @@ const dataKate2 = [10, 5, 6, 1, 4];
 checkDogs(dataJulia, dataKate);
 checkDogs(dataJulia2, dataKate2);
 */
+
+///////////////////////////////////////
+// Coding Challenge #2
+/*
+  Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the 
+    average age of the dogs in their study. 
+
+  Your tasks: 
+  Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order: 
+
+  1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. 
+    If the dog is > 2 years old, humanAge = 16 + dogAge * 4 
+  2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old) 
+  3. Calculate the average human age of all adult dogs 
+  4. Run the function for both test datasets 
+
+  Test data: 
+  • Data 1: [5, 2, 4, 1, 15, 8, 3] 
+  • Data 2: [16, 6, 10, 5, 6, 1, 4] 
+*/
+/*
+const calcAverageHumanAge = function (ages) {
+  // Task 1:
+  const dogToHumanAge = ages.map(
+    age => (age = age <= 2 ? 2 * age : 16 + age * 4)
+  );
+  console.log('dogToHumanAges:', dogToHumanAge);
+
+  // Task 2:
+  const adultAgesFiltered = dogToHumanAge.filter(humanAge => humanAge >= 18);
+  console.log('adultAgesFiltered:', adultAgesFiltered);
+
+  // Task 3:
+  const avgAdultAge = adultAgesFiltered.reduce((acc, age) => acc + age, 0) / adultAgesFiltered.length; //prettier-ignore
+  console.log('avgAdultAge:', avgAdultAge);
+};
+
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+console.log('Test Data 1:', data1);
+calcAverageHumanAge(data1);
+
+console.log('-------------------');
+
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+console.log('Test Data 2:', data2);
+calcAverageHumanAge(data2);
+*/
