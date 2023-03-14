680acb7f63e76c0c3e99a5bb24f476f02207806f
EmmanuelC1 <emcastillo@csumb.edu>
2023-03-13 22:04:11 -0700

    started notes on how DOM works (unfinished)

---
+++ 13-Advanced-DOM-Bankist/starter/notes.js
@@ -0,0 +1,8 @@
+/*
+    What is the DOM?
+        • Allows us to make JavaScript interact with the browser
+        • We can write JavaScript to create, modify and delete HTML elements. Set styles, classes and attributes; and
+            listen and respond to events.
+        • DOM tree is generated from an HTML document, which we can then interact with.
+        • DOM is a very complex API that contains lots of methods and properties to interact with the DOM tree.
+*/
