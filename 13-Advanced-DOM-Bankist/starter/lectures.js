cb649aadff38af9097b134cb6c0915ce77037bf3
EmmanuelC1 <emcastillo@csumb.edu>
2023-03-14 19:31:17 -0700

    Styles, Attributes, and Classes

--- 13-Advanced-DOM-Bankist/starter/lectures.js
+++ 13-Advanced-DOM-Bankist/starter/lectures.js
@@ -2,6 +2,7 @@
 
 ///////////////////////////////////////
 // Selecting Elements
+/*
 const header = document.querySelector('.header');
 
 const allSections = document.querySelectorAll('.section');
@@ -23,7 +24,7 @@ const message = document.createElement('div');
 message.classList.add('cookie-message');
 // message.textContent = 'We use cookies for improved functionality and analytics.';
 message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie-message">Got it<button/>';
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie-message">Got it</button>';
 
 // adds as a first child to element (inside header)
 // header.prepend(message);
@@ -47,3 +48,54 @@ document
     message.remove(); // new method
     // message.parentElement.removeChild(message); // old method to remove
   });
+*/
+
+///////////////////////////////////////
+// Styles, Attributes, and Classes
+/*
+// Styles (inline styles)
+message.style.backgroundColor = '#37393d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor); // rgb(55, 57, 61)
+console.log(message.style.color); // '' nothing because only works for inline styles (not styles set in css file)
+console.log(getComputedStyle(message).color); // rgb(187, 187, 187) Solution for non inline styles
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// console.log(message.style.height);
+
+// CSS Custom Properties (CSS Variables)
+// from root in CSS change primary color to orangered
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // Bankist Logo
+console.log(logo.src); // absolute path to img
+console.log(logo.getAttribute('src')); // relative path to img 'img/logo.png'
+console.log(logo.className); // nav__logo
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // absolute link path
+console.log(link.getAttribute('href')); // '#'
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer); // not a standard attribute so undefined
+console.log(logo.getAttribute('designer')); // Emmanuel
+logo.setAttribute('company', 'Bankist');
+
+// Data Attributes (has to start with 'data' in CSS)
+console.log(logo.dataset.versionNumber); // 3.0
+
+// Classes
+logo.classList.add('insertClassHere', 'anotherClass');
+logo.classList.remove('insertClassHere', 'anotherClass');
+logo.classList.toggle('insertClassHere');
+logo.classList.contains('insertClassHere'); // not 'includes'
+
+// DONT use since it overrides all existing classes then only allows one
+// logo.className = 'emmanuel';
+*/
