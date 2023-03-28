39c517bc9b132b9a99e3226ba572d4d6349d942f
EmmanuelC1 <emcastillo@csumb.edu>
2023-03-27 21:25:07 -0700

    Refactored code by adding App class to reflect project architecture

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,76 +11,94 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+  // Private Class Fields
+  #map;
+  #mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // get latitude and longitude ffrom geolocation
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-      const coords = [latitude, longitude];
-
-      // set the view of the map at current coordinates with the zoom of 15.5
-      map = L.map('map').setView(coords, 15.5);
-
-      // tiles that make up map with theme we chose from openstreetmap
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      // Handles clicks on map (custom event listener provided through Leaflet library)
-      map.on('click', function (event) {
-        mapEvent = event; // set event on map to mapEvent (global variable)
-        form.classList.remove('hidden'); // show workout detail form
-        inputDistance.focus(); // focus the input field for distance for better UX
-      });
-    },
-    function () {
-      alert('Could not get your current location.');
+  // Get user's geolocation
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this), // Success getting geolocation (bind this keyword on callback to app obj)
+        function () {
+          alert('Could not get your current location.'); // Error getting getting geolocation
+        }
+      );
     }
-  );
+  }
+
+  // Display Leaflet Map at geolocation
+  _loadMap(position) {
+    // get latitude and longitude from geolocation
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    // set the view of the map at current coordinates with the zoom of 15.5
+    this.#map = L.map('map').setView(coords, 15.5);
+
+    // tiles that make up map with theme we chose from openstreetmap
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // Handles clicks on map (custom event listener provided through Leaflet library)
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  // Display Workout Details Form
+  _showForm(event) {
+    this.#mapEvent = event; // set the clicked 'event' on the map to 'mapEvent' (global variable)
+    form.classList.remove('hidden');
+    inputDistance.focus(); // focus the input field for distance for better UX
+  }
+
+  // On input change of workout type (running/cycling) toggle input field cadence/elevation gain, respectively
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  // Display Marker when user submits workout detail form
+  _newWorkout(e) {
+    e.preventDefault(); // Prevent form from reloading page
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    // Get coordinates where user clicked on map
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    // Adds marker and popup with custom options on map at current coordinates
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+          content: 'Workout',
+        })
+      )
+      .openPopup();
+  }
 }
 
-/////////////////////////////////////////////////
-// Event Listeners
-
-// Display Marker when user submits workout detail form
-form.addEventListener('submit', function (e) {
-  // Prevent form from reloading page
-  e.preventDefault();
-
-  // Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-
-  // Get coordinates where user clicked on map
-  const { lat, lng } = mapEvent.latlng;
-
-  // Adds marker and popup with custom options on map at current coordinates
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-        content: 'Workout',
-      })
-    )
-    .openPopup();
-});
-
-// On input change of workout type (running/cycling) toggle input field cadence/elevation gain, respectively
-inputType.addEventListener('change', function () {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-});
+// Start app
+const app = new App();
