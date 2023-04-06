3bd20f87eba058ce14c508cded99caebc508b838
EmmanuelC1 <emcastillo@csumb.edu>
2023-04-06 08:01:00 -0700

    Coding Challenge #2

--- 16-Asynchronous/starter/codingChallenges.js
+++ 16-Asynchronous/starter/codingChallenges.js
@@ -86,3 +86,87 @@ whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 */
+
+//////////////////////////////////////////
+// Coding Challenge #2
+/*  
+  Your tasks: 
+  Tasks are not super-descriptive this time, so that you can figure out some stuff by yourself. Pretend you're working on your own 😉 
+
+  PART 1 
+  1. Create a function 'createImage' which receives 'imgPath' as an input. This function returns a promise which creates a new image (use 
+    document.createElement('img')) and sets the .src attribute to the provided image path 
+
+  2. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should 
+    be the image element itself. In case there is an error loading the image (listen for the 'error' event), reject the promise 
+
+  3. If this part is too tricky for you, just watch the first part of the solution 
+  
+  PART 2 
+  4. Consume the promise using .then and also add an error handler 
+
+  5. After the image has loaded, pause execution for 2 seconds using the 'wait' function we created earlier 
+
+  6. After the 2 seconds have passed, hide the current image (set display CSS property to 'none'), and load a second image (Hint: Use the image 
+    element returned by the 'createImage' promise to hide the current image. You will need a global variable for that 😉) 
+
+  7. After the second image has loaded, pause execution for 2 seconds again 
+
+  8. After the 2 seconds have passed, hide the current image 
+
+  Test data: 
+    Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to “Fast 3G” in the dev tools Network tab, 
+    otherwise images load too fast
+*/
+/*
+const imgContainer = document.querySelector('.images');
+let imgEl;
+
+const wait = function (seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// Task 1
+const createImage = function (imgPath) {
+  return new Promise((resolve, reject) => {
+    imgEl = document.createElement('img');
+    imgEl.src = imgPath;
+
+    // Task 2
+    imgEl.addEventListener('load', function () {
+      resolve(imgEl);
+    });
+
+    //prettier-ignore
+    imgEl.addEventListener('error', function() {
+      reject(new Error('Could not load image. Please check if image path is correct.'))
+    });
+  });
+};
+
+// Task 4
+createImage('img/img-1.jpg')
+  .then(res => {
+    imgContainer.append(res);
+    res.classList.add('images');
+    // Task 5
+    return wait(2);
+  })
+  .then(() => {
+    // Task 6
+    imgEl.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(res => {
+    // Task 7
+    imgContainer.append(res);
+    res.classList.add('images');
+    return wait(2);
+  })
+  .then(() => {
+    imgEl.style.display = 'none';
+  })
+  .catch(err => console.error(err));
+*/
