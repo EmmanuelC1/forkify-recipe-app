f148886033f565c9babf3d4faa97632a77d08767
EmmanuelC1 <emcastillo@csumb.edu>
2023-04-11 19:45:45 -0700

    Overview of Modules in JS notes

--- 17-Modern-JS-Modules-Tooling/starter/notes.js
+++ 17-Modern-JS-Modules-Tooling/starter/notes.js
@@ -1,5 +1,5 @@
 /* 
-    An Overview of Modern Javascript Development
+    Overview of Modern JavaScript Development
         –– Developement Process
             • We divide our project into mulitple modules that can share data between one another and make out code more organized
                 and maintanable.
@@ -28,5 +28,55 @@
 
         –– Production
                 • Step where the application is being used to real users in the real world.
+
+
+    Overview of Modules in JavaScript
+        –– Modules
+            • A module is a reusable piece of code that encapsulates implementaion details. Usually a standalone file, but it doesn't have to be.
+            • A module can also have imports and exports. We can export values or functions to the public API. We can impirt values from other 
+                modules, and these other modules are called dependencies of the importing module.
+        
+        –– Why Modules?
+            • Compose Software: modules are small building blocks that we out together to build complex applications.
+            • Isolate Components: modules can be developed in isolation without thinking about the entire codebase.
+            • Abstract Code: implement low-level code in modules and import these abstractions into other modules.
+            • Organized Code: modules naturally lead to more organized codebase.
+            • Reuse Code: modules allow us to easily reuse the same code, even across multiple projects.
+
+        –– Native JavaScript (ES6) Modules
+            • ES6 Modules are modules stored in files, exactly one module per file.
+
+            • ES6 Modules vs Scripts   
+
+                                    ES6 Modules             vs              Scripts
+            Top-level variables     Scoped to module                        Global
+            Default mode            Strict mode                             "Sloppy" mode
+            Top-level 'this'        undefined                               window
+            Imports and Exports     YES (need to happen at top-level        NO
+                                    imports are hoisted)
+            HTML Linking            <script type="module">                  <script>
+            File Downloading        Asynchronous                            Synchronous (unless we use async or defer attributes in script tag)
+
+        –– How ES6 Modules Are Imported
+            • During the parsing process (reading code without executing), imports are hoisted. The whole process of importing modules happens
+                before the code in the main module is actually executed.
+
+            • Importing Modules before execution
+                - Modules are imported synchronously
+                - Possible thanks to top-level ('static') imports, which make imports known before execution
+                - This makes 'bundling' and 'dead code elimination' possible
+
+            • After the Parsing process has figured out which modules it needs to import, these modules are actually downloaded from the server
+                asynchronously. It is only the importing operation itself that happends synchronously.
+
+            • After the module arrives, it's also parsed and the module exports from the exporting module are linked to the imports in the 
+                importing file. Exported values/functions are not copied to imports, they are live connections (just a reference to the export).
+                Any change in the exporting module also happends in the importing module (unique to ES6 modules)
+
+            • The code in the imported modules is then executed, and the process of importing modules is finally finished.
+            • After this process, the importing file/module can finally be excuted as well (e.g. index.js).
+
+
+
 
 */
