e28e4388ec21708c39055a9a5070da13573d7474
EmmanuelC1 <emcastillo@csumb.edu>
2023-04-12 18:56:56 -0700

    Top-level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,6 +2,7 @@
 
 ////////////////////////////////////////////
 // Exporting and Importing in ES6 Modules
+/*
 console.log('Importing Module');
 
 // Importing Module
@@ -26,3 +27,39 @@ add('apples', 4);
 
 // showing live connection with cart array changing values (we originally exported an empty array)
 console.log(cart);
+*/
+
+////////////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// We can use await outside async function (only in modules), however it blocks top-level code execution
+// console.log('Start fetching');
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+// console.log('This code will run after both top-level awaits');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  //   console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// async functions always return a Promise, so it is not the lastPost we are expecting
+const lastPost = getLastPost();
+// console.log(lastPost); // Promise {<pending>}
+
+// One work-around the Promise (not very clean)
+// lastPost.then(res => console.log(res));
+
+// Using top-level await (cleaner solution)
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+// Implication w/ top-level await (importing a module that has top-level await in it)
+// top-level await in shoppingCart.js will block any code in script.js from execution until await is fulfilled
+import * as ShoppingCart from './shoppingCart.js';
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,11 @@
 // Exporting Module
 console.log('Exporting Module');
 
+// Blocking Code
+console.log('Start fetching users');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('Finished fetching users');
+
 const shippingCost = 10;
 export const cart = [];
 
