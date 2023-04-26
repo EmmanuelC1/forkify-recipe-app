6cecb93b754544b5dfbfc4734712a60aad22c928
EmmanuelC1 <emcastillo@csumb.edu>
2023-04-25 21:40:49 -0700

    Publisher-Subscriber Design Pattern notes (Event Handling in MVC)

--- 18-forkify/starter/notes.js
+++ 18-forkify/starter/notes.js
@@ -50,4 +50,20 @@
                 3. Asking the Model for some data, might involve doing an AJAX request to the Web.
                 4. When the data arrives, the Controller takes the data and sends it to the View
                 5. The View will render that data to the UI
+
+    
+    Event Handling in MVC: Publisher-Subscriber Design Pattern
+        • Event should be handled in the controller (otherwise we would have application logic in the view)
+        • Events should be listened for in the view (otherwise we would need DOM elements in the controller)
+        
+        • Publisher: code that know WHEN to react
+        • Subscriber: code that WANTS to react
+        • Subscribe to publisher by passing in the subscriber function
+
+        • 'controlRecipes' (controller.js) will be passed into 'addHandlerRender' (recipeView.js) when program starts
+        • 'addHandlerRender' listens for events (addEventListener), and uses 'controlRecipes' as callback
+
+        • When the program starts, the init() function in the controller calls 'addHandlerRender()' in the View (possible because controller imports both
+            the model and the view) and passes 'conntrolRecipes()' (from the controller) as an argument.
+        • The 'addHandlerRender()' function listens to events (addEventListener) and uses the passed in 'controlRecipes()' function as the callback
 */
