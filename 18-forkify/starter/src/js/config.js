86d5ffe4c06af37be1e21e2183590e75b3176367
EmmanuelC1 <emcastillo@csumb.edu>
2023-04-26 17:56:44 -0700

    adds searchView that fetches recipes based on user query

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,5 +1,3 @@
-// All constant variables that are re-used across the project
-
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
-export const API_KEY = ``;
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const API_KEY = `c84c4ac9-416c-41af-a3cb-0e4b9277a105`;
 export const TIMEOUT_SEC = 10;
--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,12 +1,10 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js'; // polyfill
 import 'regenerator-runtime/runtime'; // polyfill async await
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -25,8 +23,29 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // Get user query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Render Loading Spinner
+    recipeView.renderSpinner();
+
+    // Load Search Results
+    await model.loadSearchResults(query);
+
+    // Render Search Results
+    console.log(model.state.search);
+    // recipeView.render(...model.state.search.results);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
 const init = function () {
   // Publisher-Subscriber Design Pattern
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
