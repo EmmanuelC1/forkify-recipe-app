3826650aa941fb7f424482f9261fdca89d07e3a5
EmmanuelC1 <emcastillo@csumb.edu>
2023-05-05 17:24:25 -0700

    POST request for adding recipe

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const API_KEY = `c84c4ac9-416c-41af-a3cb-0e4b9277a105`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 1.5;
