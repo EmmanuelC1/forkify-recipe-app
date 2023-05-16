9ef02b2f05e796ba22ee8e4804fd2927d42ed892
EmmanuelC1 <emcastillo@csumb.edu>
2023-05-15 20:15:59 -0700

    fixed import bug

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg'; // Parcel 2 uses 'url:' syntax
-import { fracty } from 'fracty';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -126,7 +126,9 @@ class RecipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity).toString() : ''}</div>
+        <div class="recipe__quantity">${
+          ing.quantity ? fracty(ing.quantity).toString() : ''
+        }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit} </span>${ing.description}
         </div>
